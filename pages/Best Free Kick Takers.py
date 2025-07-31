a45ca7c0335a758afe6d90db6a1e33f7f1f8e34c
Muhammad Rafay Tahir <mrafaytahir5@outlook.com>
2025-07-31 20:08:17 +0500

    Update Best Free Kick Takers.py

--- pages/Best Free Kick Takers.py
+++ pages/Best Free Kick Takers.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import streamlit as st
-import duckdb as db
 from load_data import load_data
-
-con = db.connect()
 
 players = load_data()
 
@@ -18,7 +15,7 @@ with st.container():
           acc_max = st.slider('Select the __Maximum__ free kick takers accuracy: ', min_value=players['fk_acc'].min(), max_value=players['fk_acc'].max(),key=2)
           st.form_submit_button('Submit', use_container_width=True)
      if acc_min<=acc_max:
-          df = con.execute(f'select * from players where fk_acc >= {acc_min} and fk_acc <= {acc_max}').df()
+          df = players[(players['fk_acc'] >= acc_min) & (players['fk_acc'] <= acc_max)]
           st.markdown('The Players with ___Free Kick Accuracy___ >= as you selected above: ')
           df.sort_values(by='fk_acc', ascending=False, inplace=True)
           st.dataframe(df.iloc[:10, :].reset_index().drop(columns=['index']))
